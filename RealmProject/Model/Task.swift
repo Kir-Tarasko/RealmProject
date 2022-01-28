90cdd8e32bf4fc3019b35c412c5255f4962b7946
Кирилл Тараско <tarasko.kirill@icloud.com>
2022-01-28 23:15:32 +0200

    New project with swipe actions, sorting tasks but no realm

---
+++ RealmProject/Model/Task.swift
@@ -0,0 +1,22 @@
+//
+//  Task.swift
+//  RealmProject
+//
+//  Created by Кирилл Тараско on 28.01.2022.
+//
+
+import RealmSwift
+
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
+}
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isComplete = false
+}
+
---
+++ RealmProject/Services/StorageManager.swift
@@ -0,0 +1,87 @@
+//
+//  StorageManager.swift
+//  RealmProject
+//
+//  Created by Кирилл Тараско on 28.01.2022.
+//
+
+import RealmSwift
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    let realm = try! Realm()
+    
+    private init() {}
+    
+    // MARK: - Task List
+    func save(taskLists: [TaskList]) {
+        try! realm.write {
+            realm.add(taskLists)
+        }
+    }
+    
+    func save(taskList: TaskList) {
+        write {
+            realm.add(taskList)
+        }
+    }
+    
+    func delete(taskList: TaskList) {
+        write {
+            realm.delete(taskList.tasks)
+            realm.delete(taskList)
+        }
+    }
+    
+    func edit(taskList: TaskList, newValue: String) {
+        write {
+            taskList.name = newValue
+        }
+    }
+
+    func done(taskList: TaskList) {
+        write {
+            taskList.tasks.setValue(true, forKey: "isComplete")
+            
+        }
+    }
+
+    // MARK: - Tasks
+    func save(task: Task, to taskList: TaskList) {
+        write {
+            taskList.tasks.append(task)
+        }
+    }
+    
+    private func write(completion: () -> Void) {
+        do {
+            try realm.write {
+                completion()
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func delete(task: Task) {
+        write {
+            realm.delete(task)
+            
+        }
+    }
+    
+    func edit(task: Task, name: String, note: String)  {
+        write {
+            task.name = name
+            task.note = note
+        }
+    }
+    
+    func done(task: Task) {
+        write {
+            task.isComplete.toggle()
+        }
+    }
+}
+
